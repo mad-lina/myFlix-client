3f005c142e2f6747c517ca947441c62cf2cd2db5
mad-lina <madelina.ecclesia@gmail.com>
2022-05-02 08:31:46 -0400

    create component MovieView, resolves #11

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
 
@@ -10,15 +11,22 @@ export class MainView extends React.Component {
         { id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...' },
         { id: 2, Title: 'The Shawshank Redemtion', Description: 'desc2...', ImagePath: '...' },
         { id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...' }
-      ]
-    }
+      ],
+      selectedMovie: null
+    };
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, selectedMovie } = this.state;
+
+    if (selectedMovie) {
+      return <MovieView movie={selectedMovie} />;
+    }
+
     if (movies.length === 0) {
       return <div className="main-view">This list is empty!</div>;
     }
+
     return (
       <div className="main-view">
         {movies.map((movie) => <MovieCard key={movie._id} movie={movie} />)}
