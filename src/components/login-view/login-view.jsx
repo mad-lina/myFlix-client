2c0248dfddac032bd9ead51b59e2b56dcd0e13b8
mad-lina <madelina.ecclesia@gmail.com>
2022-05-09 09:47:40 -0400

    add bootstrap styling to registration page, resolves #27

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -19,21 +19,23 @@ export function LoginView(props) {
         <Col md={6}>
           <CardGroup>
             <Card style={{ marginTop: 100 }}>
-              <Card.Title>Welcome to myFlix!</Card.Title>
-              <Form>
-                <Form.Group controlId="formUsername">
-                  <Form.Label>Username: </Form.Label>
-                  <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-                </Form.Group>
+              <Card.Body>
+                <Card.Title>Welcome to myFlix!</Card.Title>
+                <Form>
+                  <Form.Group controlId="formUsername">
+                    <Form.Label>Username: </Form.Label>
+                    <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                  </Form.Group>
 
-                <Form.Group controlId="formPassword">
-                  <Form.Label>Password: </Form.Label>
-                  <Form.Control type="text" onChange={e => setPassword(e.target.value)} />
-                </Form.Group>
+                  <Form.Group controlId="formPassword">
+                    <Form.Label>Password: </Form.Label>
+                    <Form.Control type="text" onChange={e => setPassword(e.target.value)} />
+                  </Form.Group>
 
-                <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
-                <Button>Register</Button>
-              </Form>
+                  <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+                  <Button>Register</Button>
+                </Form>
+              </Card.Body>
             </Card>
           </CardGroup>
         </Col>
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Row, Col, Card, CardGroup, Container } from 'react-bootstrap';
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState('');
@@ -14,24 +14,67 @@ export function RegistrationView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Birthday:
-        <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Create</button>
-    </form>
+    <Container>
+      <Row>
+        <Col md={6}>
+          <CardGroup>
+            <Card style={{ marginTop: 100 }}>
+              <Card.Body>
+                <Card.Title>Welcome to myFlix!</Card.Title>
+
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Username: </Form.Label>
+                    <Form.Control
+                      value={username}
+                      type="text"
+                      onChange={e => setUsername(e.target.value)}
+                      required
+                      placeholder='Enter a username'
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Password: </Form.Label>
+                    <Form.Control
+                      value={password}
+                      type="password"
+                      onChange={e => setPassword(e.target.value)}
+                      required
+                      minLength="8"
+                      placeholder='Password must be 8 characters or longer'
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Email: </Form.Label>
+                    <Form.Control
+                      value={email}
+                      type="email"
+                      onChange={e => setEmail(e.target.value)}
+                      required
+                      placeholder='Enter your email'
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Birthday </Form.Label>
+                    <Form.Control
+                      value={birthday}
+                      type="date"
+                      onChange={e => setBirthday(e.target.value)}
+                      required
+                      placeholder="What's your birthday?"
+                    />
+                  </Form.Group>
+
+                  <Button type="submit" onClick={handleSubmit}>Register</Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
