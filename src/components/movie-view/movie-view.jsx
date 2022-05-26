5a2fbc3212647fcd9e0d2fb50ae1a05dc0c0efe5
mad-lina <madelina.ecclesia@gmail.com>
2022-05-26 10:00:51 -0400

    Fix syntax

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 export class MovieView extends React.Component {
